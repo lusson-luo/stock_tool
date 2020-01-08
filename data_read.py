a79b040d30d2acf0930c6c649a6884e658b21c1b
marx_luo@foxmail.com <>
2020-01-08 10:38:49 +0800

    将股票信息抽成json文件，以文件方式保存；增加data_read类读取文件数据

---
+++ data_read.py
@@ -0,0 +1,46 @@
+import json
+
+
+class Stock:
+    name = ""
+    code = ""
+    min_threshold = 0
+    max_threshold = 0
+    hope_buy_price = 0
+    hope_sell_price = 0
+    buy_price = 0
+
+    def __init__(self, name, code, min_threshold, max_threshold, hope_buy_price=0, hope_sell_price=0, buy_price=0):
+        self.name = name
+        self.code = code
+        self.min_threshold = min_threshold
+        self.max_threshold = max_threshold
+        self.hope_buy_price = hope_buy_price
+        self.hope_sell_price = hope_sell_price
+        self.buy_price = buy_price
+
+def getShowData():
+    with open("stock_data.json", 'r') as load_f:
+        load_dict = json.load(load_f)
+        stocks = map(lambda stock: Stock(stock['name'],
+                stock['code'],                             
+                stock['min_threshold'] if stock.__contains__('min_threshold') else 0,
+                stock['max_threshold'] if stock.__contains__('max_threshold') else 0,
+                stock['hope_buy_price'] if stock.__contains__('hope_buy_price') else 0,
+                stock['hope_sell_price'] if stock.__contains__('hope_sell_price') else 0,
+                stock['buy_price'] if stock.__contains__('buy_price') else 0
+            ), load_dict['show_data'])
+        return stocks
+    return []
+
+
+def getAlarmData():
+    with open("stock_data.json", 'r') as load_f:
+        load_dict = json.load(load_f)
+        stocks = map(lambda stock: Stock(stock['name'],
+                stock['code'],                             
+                stock['min_threshold'] if stock.__contains__('min_threshold') else 0,
+                stock['max_threshold'] if stock.__contains__('max_threshold') else 0
+            ), load_dict['alarm_data'])
+        return stocks
+    return []
--- stock_alarm.py
+++ stock_alarm.py
@@ -1,18 +1,16 @@
 import urllib.request
 import pync
+import data_read
 
-stocks = [{"code": "sz000063", "name": "中兴通讯", "min_threshold": "30.6", "max_threshold": "38"},
-          {"code": "sh600036", "name": "招商银行", "min_threshold": "36", "max_threshold": "45"},
-          {"code": "sz000002", "name": "万科 A", "min_threshold": "25.8", "max_threshold": "35"},
-          {"code": "sh000016", "name": "上证50"}]
+stocks = data_read.getAlarmData()
 
 for ind, stock in enumerate(stocks):
-    if stock.__contains__('min_threshold') and stock.__contains__('max_threshold'):
-        response = str(urllib.request.urlopen(f"http://hq.sinajs.cn/list={stock['code']}").read())
+    if stock.min_threshold != 0 and stock.max_threshold != 0:
+        response = str(urllib.request.urlopen(f"http://hq.sinajs.cn/list={stock.code}").read())
         stockData = response.split("\"")[1].split(",")
-        priceNow = stockData[3]
-        if priceNow != '0.000':
-            if stock['min_threshold'] > priceNow:
-                pync.notify(f'{stock["name"]}当前价格{priceNow}', title=f'{stock["name"]}股票已低于设定值{stock["min_threshold"]}')
-            if stock['max_threshold'] < priceNow:
-                pync.notify(f'{stock["name"]}当前价格{priceNow}', title=f'{stock["name"]}股票已高于设定值{stock["max_threshold"]}')
+        priceNow = float(stockData[3])
+        if priceNow != 0.000:
+            if stock.min_threshold > priceNow:
+                pync.notify(f'{stock.name}当前价格{priceNow}', title=f'{stock.name}股票已低于设定值{stock.min_threshold}')
+            if stock.max_threshold < priceNow:
+                pync.notify(f'{stock.name}当前价格{priceNow}', title=f'{stock.name}股票已高于设定值{stock.max_threshold}')
